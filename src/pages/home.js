0f3547690c0569d11227a1fdb08184067d32a9d8
Mariano Soffredi <nanospe2k2@gmail.com>
2023-03-04 14:10:14 -0300

    new file:   src/pages/home.js

---
+++ src/pages/home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState } from "react";
+
+import Sidebar from "../sidebar/sidebar";
+import Todos from "../todos/todos";
+import filters from "../sidebar/filters";
+
+export default function Home() {
+  const [filter, setFilter] = useState(() => filters[0].filter);
+  const [currModal, setModal] = useState(null);
+
+  function changeFilter(selectedFilter) {
+    setFilter(() => selectedFilter);
+  };
+
+  function updateModal(modal) {
+    setModal(modal);
+  };
+
+  return (
+    <>
+      <label key='modal'>
+        {currModal}
+      </label>
+      <Sidebar key='sidebar' filters={filters} filterChange={changeFilter}/>
+      <Todos key='todos' modal={updateModal} selectedFilter={filter}/>
+    </>
+  );
+}
